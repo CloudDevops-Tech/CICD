31893e466cb7897315d180ba0e32cd9aec031225
lokesh <lokeshyajjavarapu17@gmail.com>
2026-04-07 18:55:52 +0530

    Day2_PythonApp_From_Github_to_EC2_Using_Jenkins

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello from EC2")
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from EC2!"
+
+if __name__ == "__main__":
+    # 0.0.0.0 means listen on all network interfaces
+    app.run(host="0.0.0.0", port=5000)
